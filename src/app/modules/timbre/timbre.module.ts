44e64cd3af29320652a0f1375f056489f06d3436
alexandre <alexandre.lefaix@groupe-zephir.fr>
2025-04-29 17:25:04 +0200

    Mode application

--- src/app/modules/timbre/timbre.module.ts
+++ src/app/modules/timbre/timbre.module.ts
@@ -30,34 +30,40 @@ import {TimbreRechercheComponent} from "./components/recherche/timbre-recherche.
 import {TimbreResultatTableComponent} from "./components/resultat-table/timbre-resultat-table.component";
 import {TimbreTotalComponent} from "./components/total/timbre-total.component";
 import {TimbreService} from "../../shared/services/timbre/timbre.service";
+import {provideServiceWorker} from "@angular/service-worker";
+import {environment} from "../../../environments/environment";
 
 @NgModule({
 	declarations: [
 		TimbreLayoutComponent, TimbreModifierComponent, TimbreImporterComponent, TimbreResultatTableComponent, TimbreResultatComponent, TimbreResumeComponent, TimbreRechercheComponent, TimbreTotalComponent
 	],
-    imports: [
-        MaterialModule,
-        TimbreRoutingModule,
-        CommonModule,
-        FlexModule,
-        FlexLayoutModule,
-        FormsModule,
-        AsyncPipe,
-        LibSpinnerModule,
-        LibIconModule,
-        LibDialogImageModule,
-        LibLibelleModelModule,
-        DirectiveModule,
-        ErrorModule,
-        PipeModule,
-        LibModalModule,
-        LibUploadModule,
-        SimpleNotificationModule,
-        MenuModule,
-        ReactiveFormsModule
-    ],
+	imports: [
+		MaterialModule,
+		TimbreRoutingModule,
+		CommonModule,
+		FlexModule,
+		FlexLayoutModule,
+		FormsModule,
+		AsyncPipe,
+		LibSpinnerModule,
+		LibIconModule,
+		LibDialogImageModule,
+		LibLibelleModelModule,
+		DirectiveModule,
+		ErrorModule,
+		PipeModule,
+		LibModalModule,
+		LibUploadModule,
+		SimpleNotificationModule,
+		MenuModule,
+		ReactiveFormsModule
+	],
 	exports: [],
-	providers: [TimbreService, UtilsService, TimbreUtilsService, TimbreBlocService, TimbreResumeService, UploadService, provideHttpClient()]
+	providers: [TimbreService, UtilsService, TimbreUtilsService, TimbreBlocService, TimbreResumeService, UploadService, provideHttpClient(),
+		provideServiceWorker('ngsw-worker.js', {
+			enabled: environment.production,
+			registrationStrategy: 'registerWhenStable:30000'
+		})]
 })
 export class TimbreModule {
 }
