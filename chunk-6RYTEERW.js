dbff64bb2cad4bb2319ab49d70f7c72cdac32b97
alexandre <alexandre.lefaix@groupe-zephir.fr>
2025-05-05 11:38:59 +0200

    Auto-generated commit

---
+++ chunk-6RYTEERW.js
@@ -0,0 +1,1 @@
+import{I as i,N as o}from"./chunk-SR36DNPU.js";var t=class r{static \u0275fac=function(e){return new(e||r)};static \u0275mod=o({type:r});static \u0275inj=i({})};export{t as a};
--- chunk-CAOUZ4P7.js
+++
@@ -1,1 +0,0 @@
-import{I as i,N as o}from"./chunk-XASVH4YM.js";var t=class r{static \u0275fac=function(e){return new(e||r)};static \u0275mod=o({type:r});static \u0275inj=i({})};export{t as a};
