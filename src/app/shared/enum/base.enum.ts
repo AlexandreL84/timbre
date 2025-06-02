6eb5c08a927922354d98b1465a11125b2b50e072
alexandre <alexandre.lefaix@groupe-zephir.fr>
2025-06-02 17:59:11 +0200

    Correction enum

--- src/app/shared/enum/base.enum.ts
+++ src/app/shared/enum/base.enum.ts
@@ -1,7 +1,7 @@
 export enum BaseEnum {
 	TIMBRE = "/timbres",
 	TIMBRE_ACQUIS = "/timbres_acquis",
-	TIMBRE_BLOC = "/timbres_bloc",
+	TIMBRE_BLOC = "/timbres_bloc2",
 	TIMBRE_BLOC_ACQUIS = "/timbres_bloc_acquis",
 	PAYS = "/timbres_pays",
 	USER = "/user",
